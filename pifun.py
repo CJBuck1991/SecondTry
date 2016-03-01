150ee73be19a321b2982ae539123ea23a2544e81
CJBuck1991 <repgrpfin1991@gmail.com>
2016-03-01 11:06:48 -0600

    Update pifun.py

--- pifun.py
+++ pifun.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     print("Comparing 10000000 terms for Pisquare using integer vs float")
     z=pifun(10000000)
     zz=piintfun(10000000)
-    print("%23.21f       \n%22u" % (z*z,zz))
+    print("%23.21f       \n %22u" % (z*z,zz))
