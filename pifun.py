57a055e9b6ab4579fd4b127c89644daecfe81128
CJBuck1991 <repgrpfin1991@gmail.com>
2016-03-01 10:45:37 -0600

    Update pifun.py
    Added integer arithmetic comparison

--- pifun.py
+++ pifun.py
@@ -6,9 +6,21 @@ def pifun(n): # Euler series
         pisqest+=6/(i*i) # add each series term
     return math.sqrt(pisqest) # take square root
     
+def piintfun(n):
+    pisqest=0;
+    for i in range(1,n+1):
+        pisqest+=6000000000000000000000//(i*i)
+    return pisqest    
+    
 if __name__ == "__main__":
     print("Number_of_Terms   Estimate")
-    for n in (100,200,500,1000,2000,5000,10000,20000,50000,100000,10000000):
+    for n in (100,200,500,1000,2000,5000,10000,20000,50000,100000):
         print("%10u          %13.11f" % (n,pifun(n)))   
     print(" ")
+    print("system value of pi is")
     print("%13.11f" % math.pi)
+    print(" ")
+    print("Comparing 10000000 terms for Pisquare using integer vs float")
+    z=pifun(10000000)
+    zz=piintfun(10000000)
+    print("%23.21f      %22u" % (z*z,zz))
